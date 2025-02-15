ce97eacab4a76c25a1df05747278b16057dc3433
Mangercry <exnessnom@gmail.com>
2025-02-15 18:09:41 +0100

    Add files via upload

---
+++ Input.sql
@@ -0,0 +1,9 @@
+Input adresse IP de l'APP:1.2.9.88
+Input url:https//:www.mangercry.com
+Input titre de l'APP: Manger
+Input menu:Un arrière plan sur le premier plan.
+Input balance: 0-9 
+Input message : Vous avez envoyé un montant de @montant.
+Input montant :[0-9×[Monnaie : Nombre Logo: Drapeau] Couleur :Bleu]
+Input compte : [IBAN, MTCN, Numéro mobile, Numéro prépayé, Cryptomonnaie]
+Bouton payer: Payer
